928c6560a92c8e9e6f012d62785fa1da5c8a7291
E Harrison <har16020@byui.edu>
2024-09-16 17:18:49 -0700

    Fixed ChatMixin class to check ALL criteria against the incoming message..added class definition and changed from Map<s, t> to ArrayList<Chat> to fix

--- common/src/main/java/de/erdbeerbaerlp/dcintegration/architectury/mixin/ChatMixin.java
+++ common/src/main/java/de/erdbeerbaerlp/dcintegration/architectury/mixin/ChatMixin.java
@@ -11,36 +11,92 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 @Mixin(value= ServerGamePacketListenerImpl.class)
 public class ChatMixin {
 
     // Keep record of messages in memory
-    Map<String, LocalDate> lastPlayerMessages = new HashMap<>();
+    private List<SimpleChatMessage> lastPlayerMessages = new ArrayList<>();
 
     /**
      * Handle chat messages
      */
     @Redirect(method = "broadcastChatMessage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/network/chat/ChatType$Bound;)V"))
     public void chatMessage(PlayerList instance, PlayerChatMessage signedMessage, ServerPlayer sender, ChatType.Bound bound) {
-        if (ignoreDuplicateMessagesDaily(sender)) return;
+        if (// TODO: insert check against config to enable/disable checking for duplicates &&
+            ignoreDuplicateMessagesDaily(signedMessage, sender)) {
+            // ignore message if duplicate is found
+            return;
+        }
         signedMessage = DiscordIntegrationMod.handleChatMessage(signedMessage, sender);
-        instance.broadcastChatMessage( signedMessage, sender, bound);
+        instance.broadcastChatMessage(signedMessage, sender, bound);
     }
 
-    private boolean ignoreDuplicateMessagesDaily(ServerPlayer player) {
-        LocalDate today = LocalDate.now();
-        String playerId = player.getTabListDisplayName().toString();
+    private boolean ignoreDuplicateMessagesDaily(PlayerChatMessage signedMessage, ServerPlayer sender) {
+        SimpleChatMessage msg = new SimpleChatMessage(signedMessage, sender);
 
-        if (lastPlayerMessages.containsKey(playerId) && lastPlayerMessages.get(playerId).isEqual(today)) {
+        if (foundMatchingChat(msg)) {
             // A message was already sent today
             return true;
         }
 
-        // This is the first message today
-        lastPlayerMessages.put(playerId, today);
+        // While we're at it, let's clean up any older messages
+        // (Due to foundMatchingChat's logic, messages will only be cleaned
+        // when the first message of the day is sent)
+        // before adding any new ones...
+        // just so that there are for sure no accidental removals.
+        lastPlayerMessages.removeIf(element -> !element.matchesToday());
+
+        // This is the first unique instance of this message today,
+        // so we will go ahead and add it to the list.
+        lastPlayerMessages.add(msg);
         return false;
+    }
+
+    private boolean foundMatchingChat(SimpleChatMessage msg) {
+        // Filter potential duplicates to check
+        List<SimpleChatMessage> filteredChats = lastPlayerMessages
+            .stream()
+            // existing date matches today
+            .filter(element -> element.matchesToday())
+            // incoming playerID matches existing
+            .filter(element -> element.matchesPlayerID(msg))
+            // incoming message matches existing
+            .filter(element -> element.matchesMessage(msg))
+            .toList();
+
+        return !filteredChats.isEmpty();
+    }
+
+    private class SimpleChatMessage {
+        // properties
+        private String playerID = new String();
+        private String message = new String();
+        private LocalDate date = LocalDate.now();
+
+        // constructor
+        public SimpleChatMessage(PlayerChatMessage signedMessage, ServerPlayer sender) {
+            playerID = sender.getTabListDisplayName().toString();
+            message = signedMessage.signedContent();
+        }
+
+        // accessors
+        public String getPlayerID() { return playerID; }
+        public String getMessage() { return message; }
+
+        // match helpers
+        public boolean matchesPlayerID(SimpleChatMessage msg) {
+            return playerID == msg.getPlayerID();
+        }
+
+        public boolean matchesToday() {
+            return LocalDate.now().isEqual(date);
+        }
+
+        public boolean matchesMessage(SimpleChatMessage msg) {
+            return message == msg.getMessage();
+        }
     }
 }
